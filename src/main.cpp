9d326808ddf401fe6e4b07ec8797893a686c7226
napapon-wongkom <dot.wongkom@gmail.com>
2023-12-20 23:18:12 +0700

    main

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
 #include <Arduino.h>
-
+#include <WiFi.h>
+#include <MQTT.h>
+#include <string.h>
+//////////////////////////////////////////////////////////////////////////////
 #define STEP_PIN_1 19
 #define STEP_PIN_2 18
 #define STEP_PIN_3 17
@@ -8,7 +11,37 @@
 //Variable Zone//
 String Key = "";  
 int mem=0;
+const char ssid[] = "Dxt-iphone";
+const char pass[] = "magicwireless";
+
+const char mqtt_broker[]="test.mosquitto.org";
+const char mqtt_topic[]="group1.43/control";
+const char mqtt_publish[]="group1.43/recieve";
+const char mqtt_client_id[]="arduino_group_1.43"; // must change this string to a unique value
+int MQTT_PORT=1883;
+
+WiFiClient net;
+MQTTClient client;
 /////////////////////////////////////////////////////////////////////////////
+void connect() {
+  Serial.print("checking wifi...");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(1000);
+  }
+
+  Serial.print("\nconnecting...");
+  while (!client.connect(mqtt_client_id)) {  
+    Serial.print(".");
+    delay(1000);
+  }
+
+  Serial.println("\nconnected!");
+
+  client.subscribe(mqtt_topic);
+  // client.unsubscribe("/hello");
+}
+
 void Step_Drive(){
     digitalWrite(STEP_PIN_1,HIGH);
     digitalWrite(STEP_PIN_2,LOW);
@@ -85,75 +118,85 @@ int set_zero(int mem){
   }
   //Serial.println("Reverse");
 }
-//////////////////////////////////////////////////////////////////////////////
-void setup(){
-    
-}
-//////////////////////////////////////////////////////////////////////////////
-void loop(){  
-    pinMode(STEP_PIN_1,OUTPUT);
-    pinMode(STEP_PIN_2,OUTPUT);
-    pinMode(STEP_PIN_3,OUTPUT);
-    pinMode(STEP_PIN_4,OUTPUT);
-  
-    Serial.begin(9600);
-    Serial.println("Enter Key:");  
-    while (Serial.available() == 0)   { }  
-    Key = Serial.readString(); //Reading the Input string from Serial port.  
-    
-    Serial.println("-------------------------"); //Showing the details 
-    ////////////////////////////////////////////////////////////////////////
-    if(Key == "4"){
+
+void messageReceived(String &topic, String &payload) {
+  Serial.println("incoming: " + topic + " - " + payload);
+  ////////////////////////////////////////////Subscribe here!//////////////////////////////////////////////
+  if(String(payload) == "4"){
       set_zero(mem);
       Serial.println("90 Degree");
+      client.publish(mqtt_publish,"90 Degree");
       for (int i = 0; i < 128; i++)
       {
         Step_Drive();
       }
       mem = 4;
       
-    }else if (Key == "3")
+    }else if (String(payload) == "3")
     {
       Serial.println("60 Degree");
       set_zero(mem);
+      client.publish(mqtt_publish,"60 Degree");
       for (int i = 0; i < 85; i++)
       {
         Step_Drive();
       }
       mem = 3;
-    }else if(Key == "2"){
+    }else if(String(payload) == "2"){
       Serial.println("45 Degree");
+      client.publish(mqtt_publish,"45 Degree");
       set_zero(mem);
       for (int i = 0; i < 64; i++)
        {
          Step_Drive();
        }
       mem = 2;
-    }else if(Key == "1"){
+    }else if(String(payload) == "1"){
       Serial.println("30 Degree");
+      client.publish(mqtt_publish,"30 Degree");
       set_zero(mem);
       for (int i = 0; i < 43; i++)
       {
         Step_Drive();
       }
       mem = 1;
-    }else if(Key == "0"){
+    }else if(String(payload) == "0"){
       Serial.println("0 Degree");
+      client.publish(mqtt_publish,"0 Degree");
       set_zero(mem);
       mem = 0;
-    }else if(Key == "00"){
+    }else if(String(payload) == "00"){
       Serial.println("Terminate");
+      client.publish(mqtt_publish,"Terminated!");
       set_zero(mem);
       while (true){
 
       }
       mem = 0;
       
-    }else{
-      Serial.println("Wrong Key!");
     }
-    /////////////////////////////////////////////////////////////////////////
-    while (Serial.available() == 0) {}    
+}
+//////////////////////////////////////////////////////////////////////////////
+void setup(){
+    pinMode(STEP_PIN_1,OUTPUT);
+    pinMode(STEP_PIN_2,OUTPUT);
+    pinMode(STEP_PIN_3,OUTPUT);
+    pinMode(STEP_PIN_4,OUTPUT);
+    Serial.begin(9600);
+    WiFi.begin(ssid, pass);
+    client.begin(mqtt_broker, MQTT_PORT, net);
+    client.onMessage(messageReceived);
+
+    connect();
+}
+//////////////////////////////////////////////////////////////////////////////
+void loop(){  
+    client.loop();
+    delay(10);  // <- fixes some issues with WiFi stability
+    
+    if (!client.connected()) {
+      connect();
+    }
 }  
   
 
